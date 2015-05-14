895e290d93de6b46e1b5f9e2ed7e40130e10d079
eagle-dan1349 <eagle.dan.1349@gmail.com>
2015-05-14 16:48:24 +0600

    Fixing issue with json podspec

---
+++ Pod/FBSDKShareKit-RACExtensions/FBSDKShareDialog+RACExtensions.h
@@ -0,0 +1,13 @@
+//
+//  FBSDKShareDialog+RACExtensions.h
+//  Pods
+//
+//  Created by Daniil Orlov on 5/14/15.
+//
+//
+
+#import "FBSDKShareDialog.h"
+
+@interface FBSDKShareDialog (RACExtensions)
+
+@end
---
+++ Pod/FBSDKShareKit-RACExtensions/FBSDKShareDialog+RACExtensions.m
@@ -0,0 +1,13 @@
+//
+//  FBSDKShareDialog+RACExtensions.m
+//  Pods
+//
+//  Created by Daniil Orlov on 5/14/15.
+//
+//
+
+#import "FBSDKShareDialog+RACExtensions.h"
+
+@implementation FBSDKShareDialog (RACExtensions)
+
+@end
